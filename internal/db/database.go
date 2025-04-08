e9d23cb06018cd5e50fa800f61ce5b45a8e2b773
asylzhan.agataeev <asylzhan.agataeev@ffin.mobi>
2025-04-08 08:31:01 +0500

    some fixes

--- internal/db/database.go
+++ internal/db/database.go
@@ -42,7 +42,8 @@ func GetUserCity(chatID int64) (string, error) {
 
 // SavePrayer отмечает выполнение намаза
 func SavePrayer(chatID int64, prayer string) error {
-	completedAt := time.Now().UTC().Format("2006-01-02 15:04:05.000000")
+	location := time.FixedZone("UTC+5", 5*60*60)
+	completedAt := time.Now().In(location).UTC().Format("2006-01-02 15:04:05.000000")
 	_, err := DB.Exec(context.Background(),
 		"INSERT INTO prayers (chat_id, prayer_name, completed_at) VALUES ($1, $2, $3)", chatID, prayer, completedAt)
 	return err
